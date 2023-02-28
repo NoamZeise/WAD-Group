28524a88089ca90ee24176702fa960c0bc4f4690
“Sujay <2672826P@student.gla.ac.uk>
2023-02-28 23:35:45 +0000

    login and logout views added

--- froggr/views.py
+++ froggr/views.py
@@ -1,5 +1,15 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 # Create your views here.
 
@@ -14,3 +24,57 @@ def test(request):
 
 def dog(request):
     return HttpResponse("This is the dog page")
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'froggr/register.html',
+                  context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('froggr:home'))
+            else:
+                return HttpResponse("Your Froggr account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/frog-in.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('froggr:home'))
+
+
