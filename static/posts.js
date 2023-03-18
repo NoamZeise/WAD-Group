f59f1614d83c6527eb53ff64b7bc90a3d1e20de4
Timothy Martin <timothykeitaro@gmail.com>
2023-03-18 16:50:04 +0000

    put frogg.js back in, like button fix

--- static/posts.js
+++ static/posts.js
@@ -19,16 +19,5 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-    $('#like_btn').click(function() {
-        var postid;
-        postid = $(this).attr('data-postid');
-        $.get("/like-post/",
-            {'post_id': postid},
-            function(data) {
-                $('#like_count').html(data);
-                $('#like_btn').hide();
-            })
-    });
-});
+
 
