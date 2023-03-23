18ee85af363b2292ac73b8edb9361f3fbb1a3d32
NoamZeise <noam4134@gmail.com>
2023-03-23 18:11:52 +0000

    changed btn color on like/dislike

--- static/frogg.js
+++ static/frogg.js
@@ -7,13 +7,15 @@ $(document).ready(function() {
       {'post_id': postid, 'action': action},
       function(data) {
         if (action == 'like') {
-          $('#like_btn').text('Unlike Post');
-          $('#like_btn').attr('data-action', 'dislike');
-          $('#like_count').text(parseInt($('#like_count').text()) + 1);
+            $('#like_btn').text('Unlike Post');
+	    $('#like_btn').removeClass("btn-primary").addClass("btn-secondary");
+            $('#like_btn').attr('data-action', 'dislike');
+            $('#like_count').text(parseInt($('#like_count').text()) + 1);
         } else {
-          $('#like_btn').text('Like Post');
-          $('#like_btn').attr('data-action', 'like');
-          $('#like_count').text(parseInt($('#like_count').text()) - 1);
+            $('#like_btn').text('Like Post');
+	    $('#like_btn').removeClass("btn-secondary").addClass("btn-primary");
+            $('#like_btn').attr('data-action', 'like');
+            $('#like_count').text(parseInt($('#like_count').text()) - 1);
         }
       })
   });
